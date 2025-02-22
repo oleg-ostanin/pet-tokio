02eb1cfd1c0ebf4d4fb23781e78fc2111f82fdec
oleg.ostanin <oleg.ostanin@edna.ru>
2025-02-22 11:59:24 +0300

    refactor handle order

--- crates/libs/lib-core/src/task/order.rs
+++ crates/libs/lib-core/src/task/order.rs
@@ -1,15 +1,20 @@
 use std::ops::Deref;
 use std::sync::Arc;
-
+use sqlx::types::Json;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tracing::info;
 
-use lib_dto::order::{OrderId, OrderItem, OrderItemExt};
+use lib_dto::order::{OrderContent, OrderId, OrderItem, OrderItemExt};
 
 use crate::bmc::book_info::BookBmc;
 use crate::bmc::storage::StorageBmc;
 use crate::context::app_context::ModelManager;
 use crate::notify::order::OrderPayload;
+
+enum CheckResult {
+    Enough,
+    NotEnough(Vec<OrderItem>),
+}
 
 pub async fn handle_order(
     app_context: Arc<ModelManager>,
@@ -20,28 +25,44 @@ pub async fn handle_order(
     while let Some(payload) = order_rx.recv().await {
         info!("received payload is {:?}", &payload);
 
-        let mut not_enough: Vec<OrderItem> = vec![];
-
-        let order_content = payload.content().content();
-        for order_item in order_content {
-            let book_id = order_item.book_id();
-            let book_storage_info = StorageBmc::get_quantity(app_context.deref(), book_id).await;
-            if let Ok(book_storage_info) = book_storage_info {
-                info!("book_storage_info is {:?}", book_storage_info);
-                let quantity = book_storage_info.quantity().unwrap_or(0);
-                if quantity < order_item.quantity() {
-                    let diff = order_item.quantity() - quantity;
-                    let order_item_ext = OrderItem::new(book_id, diff);
-                    not_enough.push(order_item_ext);
-                }
-            }
-        }
+        let order_content = payload.content();
         let order_id = OrderId::new(payload.order_id());
 
-        if !not_enough.is_empty() {
-            storage_tx.send((order_id, not_enough)).await.unwrap()
-        } else {
-            delivery_tx.send(order_id).await.unwrap()
+        match check_if_enough(app_context.clone(), order_content).await {
+            CheckResult::NotEnough(items) => {
+                storage_tx.send((order_id, items)).await.unwrap();
+            }
+            CheckResult::Enough => {
+                delivery_tx.send(order_id).await.unwrap();
+            }
         }
+    }
+}
+
+async fn check_if_enough(
+    app_context: Arc<ModelManager>,
+    order_content: &Json<OrderContent>,
+) -> CheckResult {
+    let mut not_enough: Vec<OrderItem> = vec![];
+
+    let order_content = order_content.content();
+    for order_item in order_content {
+        let book_id = order_item.book_id();
+        let book_storage_info = StorageBmc::get_quantity(app_context.deref(), book_id).await;
+        if let Ok(book_storage_info) = book_storage_info {
+            info!("book_storage_info is {:?}", book_storage_info);
+            let quantity = book_storage_info.quantity().unwrap_or(0);
+            if quantity < order_item.quantity() {
+                let diff = order_item.quantity() - quantity;
+                let order_item_ext = OrderItem::new(book_id, diff);
+                not_enough.push(order_item_ext);
+            }
+        }
+    }
+
+    if not_enough.is_empty() {
+        CheckResult::Enough
+    } else {
+        CheckResult::NotEnough(not_enough)
     }
 }
